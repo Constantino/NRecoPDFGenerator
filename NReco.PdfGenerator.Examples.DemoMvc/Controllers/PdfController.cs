66adeff0c871b345e0273f4eb04dc42a4bef59e5
AldoSaucedo <aldo_saucedo13@hotmail.com>
2017-09-06 12:51:15 -0500

    Added style

--- NReco.PdfGenerator.Examples.DemoMvc/Controllers/PdfController.cs
+++ NReco.PdfGenerator.Examples.DemoMvc/Controllers/PdfController.cs
@@ -13,41 +13,61 @@ using System.Text.RegularExpressions;
 using System.Web.UI;
 
 using System.Web.Security;
-
 using NReco.PdfGenerator;
 
 namespace Controllers {
-	
-	public class PdfController : Controller {
+    
+    public class PdfController : Controller {
 
-		public ActionResult DemoPage() {
-			return View();
-		}
+        public ActionResult DemoPage() {
+            return View();
+        }
 
-		[ValidateInput(false)]
-		public ActionResult GeneratePdf(string htmlContent, string htmlUrl) {
+        public void PreProcessFile(string filePath){
 
-			string[] htmlFiles = htmlUrl.Split(';');
+            const string style = "<style>body { height:203mm; width:267mm; margin-left:auto; margin-right:auto; }</style>";
+            string HTMLCode = System.IO.File.ReadAllText(filePath);
+            HTMLCode = HTMLCode.Replace("<head>", "<head>" + style);
 
-			var htmlToPdf = new HtmlToPdfConverter();
+            // Open the file.
+            using (FileStream fs = System.IO.File.OpenWrite(filePath))
+            {
+                Byte[] info =
+                    new UTF8Encoding(true).GetBytes(HTMLCode);
 
-			var pdfContentType = "application/pdf";
-			Stream output = new System.IO.MemoryStream();
-			
-			string pdfName = Path.GetTempPath()+"/firstPdf.pdf";
-			if (!String.IsNullOrEmpty(htmlUrl)) {
-				//return File( htmlToPdf.GeneratePdfFromFile(htmlUrl, null), pdfContentType);
-				htmlToPdf.GeneratePdfFromFiles(htmlFiles, null, pdfName);
-				return File(pdfName, pdfContentType);
-				
-			} else {
-				return File( htmlToPdf.GeneratePdf(htmlContent, null), pdfContentType);
-			}
-		}
+                // Add some information to the file.
+                fs.Write(info, 0, info.Length);
+            }
+        }
+
+        [ValidateInput(false)]
+        public ActionResult GeneratePdf(string htmlContent, string htmlUrl) {
+
+            string[] htmlFiles = htmlUrl.Split(';');
+
+            foreach (var e in htmlFiles) {
+                PreProcessFile(e);
+            }
+
+            var htmlToPdf = new HtmlToPdfConverter();
+
+            var pdfContentType = "application/pdf";
+            Stream output = new System.IO.MemoryStream();
+            
+            string pdfName = Path.GetTempPath()+"/firstPdf.pdf";
+            if (!String.IsNullOrEmpty(htmlUrl)) {
+                //return File( htmlToPdf.GeneratePdfFromFile(htmlUrl, null), pdfContentType);
+                htmlToPdf.GeneratePdfFromFiles(htmlFiles, null, pdfName);
+                return File(pdfName, pdfContentType);
+                
+            } else {
+                return File( htmlToPdf.GeneratePdf(htmlContent, null), pdfContentType);
+            }
+        }
 
 
 
 
 
-	}
+    }
 }
